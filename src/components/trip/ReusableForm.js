9ccf7009b77e7b7585aa240f267e3c85c4424588
Christen Weston <paleeserecycle@gmail.com>
2022-07-30 12:20:42 -0700

    Updated location of trip days

--- src/components/trip/ReusableForm.js
+++ src/components/trip/ReusableForm.js
@@ -62,88 +62,94 @@ const ReusableForm = (props) => {
 
   return (
     <React.Fragment>
-      <form onSubmit={addNewTrip}>
-        <input required
-          type="text"
-          className="form-control"
-          name="tripName"
-          placeholder="Name your trip"
-          value={tripName}
-          onChange={(e) => setTripName(e.target.value)} />
-        <input required
-          type="date"
-          className="form-control"
-          name="startDate"
-          placeholder="First day of trip"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)} />
-        <input required
-          type="date"
-          className="form-control"
-          name="endDate"
-          placeholder="Last day of trip"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)} />
-        <input required
-          type="text"
-          className="form-control"
-          name="startLocation"
-          placeholder="Starting location"
-          value={startLocation}
-          onChange={(e) => setStartLocation(e.target.value)} />
-        <input required
-          type="text"
-          className="form-control"
-          name="endLocation"
-          placeholder="Ending location"
-          value={endLocation}
-          onChange={(e) => setEndLocation(e.target.value)} />
-        <input required
-          type="number"
-          className="form-control"
-          min={1}
-          name="numberOfDays"
-          placeholder="Trip length (days)"
-          value={numberOfDays}
-          onChange={(e) => setNumberOfDays(e.target.value)} />
-        <input required
-          type="string"
-          className="form-control"
-          min={1}
-          name="waypoints"
-          placeholder="waypoint"
-          value={wayPoints}
-          onChange={(e) => setWaypoints(e.target.value)} />
-        {inputFields.map(inputField => (
-          <div key={inputField.id}>
-            <label>Food Stop</label>
-            <input
-              name="foodStop"
-              className="form-control"
-              label="Food Stop"
-              variant="filled"
-              placeholder="Where to eat"
-              value={inputField.foodStop}
-              onChange={event => handleChangeInput(inputField.id, event)}
-            />
-            <label>Adventure Stop</label>
-            <input
-              name="adventureStop"
-              className="form-control"
-              label="Adventure Stop"
-              variant="filled"
-              placeholder="Where to have fun"
-              value={inputField.adventureStop}
-              onChange={event => handleChangeInput(inputField.id, event)}
-            />
-            <button className="btn btn-warning btn-sm" disabled={inputFields.length === 1} onClick={() => handleRemoveFields(inputField.id)}>Remove Day</button>
-            <button className="btn btn-light btn-sm" onClick={handleAddFields}>Add day</button>
-          </div>
-        ))}
-        <br />
-        <button className="btn btn-primary btn-lg" onSubmit={addNewTrip}>Save trip</button>
-        <br />
-      </form>
+      <div class="container">
+        <div className="text-center">
+          <h1>Planning Time</h1>
+        </div>
+        <form onSubmit={addNewTrip}>
+          <input required
+            type="text"
+            className="form-control"
+            name="tripName"
+            placeholder="Name your trip"
+            value={tripName}
+            onChange={(e) => setTripName(e.target.value)} />
+          <input required
+            type="date"
+            className="form-control"
+            name="startDate"
+            placeholder="First day of trip"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)} />
+          <input required
+            type="date"
+            className="form-control"
+            name="endDate"
+            placeholder="Last day of trip"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)} />
+          <input required
+            type="text"
+            className="form-control"
+            name="startLocation"
+            placeholder="Starting location"
+            value={startLocation}
+            onChange={(e) => setStartLocation(e.target.value)} />
+          <input required
+            type="text"
+            className="form-control"
+            name="endLocation"
+            placeholder="Ending location"
+            value={endLocation}
+            onChange={(e) => setEndLocation(e.target.value)} />
+          <input required
+            type="number"
+            className="form-control"
+            min={1}
+            name="numberOfDays"
+            placeholder="Trip length (days)"
+            value={numberOfDays}
+            onChange={(e) => setNumberOfDays(e.target.value)} />
+          {inputFields.map(inputField => (
+            <div key={inputField.id}>
+              <label>Food Stop</label>
+              <input
+                name="foodStop"
+                className="form-control"
+                label="Food Stop"
+                variant="filled"
+                placeholder="Where to eat"
+                value={inputField.foodStop}
+                onChange={event => handleChangeInput(inputField.id, event)}
+              />
+              <label>Adventure Stop</label>
+              <input
+                name="adventureStop"
+                className="form-control"
+                label="Adventure Stop"
+                variant="filled"
+                placeholder="Where to have fun"
+                value={inputField.adventureStop}
+                onChange={event => handleChangeInput(inputField.id, event)}
+              />
+              <button className="btn btn-warning btn-sm" disabled={inputFields.length === 1} onClick={() => handleRemoveFields(inputField.id)}>Remove Day</button>
+              <button className="btn btn-light btn-sm" onClick={handleAddFields}>Add day</button>
+            </div>
+          ))}
+          <input required
+            type="string"
+            className="form-control"
+            min={1}
+            name="waypoints"
+            placeholder="Stopover location"
+            value={wayPoints}
+            onChange={(e) => setWaypoints(e.target.value)} />
+
+          <br />
+          <button className="btn btn-primary btn-lg" onSubmit={addNewTrip}>Save trip</button>
+          <br />
+        </form>
+      </div>
       <br />
     </React.Fragment>
   );
